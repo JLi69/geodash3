3ee5cc59e50993e9e919192c378c9eb5a0c09f0c
jli <coderguy50@gmail.com>
2023-04-02 13:43:24 -0500

    added window icons

--- src/Init/init.cpp
+++ src/Init/init.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include "init.h"
 #include <cstdlib>
+#include <SOIL2/SOIL2.h>
 
 void Geodash3::init(GLFWwindow *&win, const char *title)
 {
@@ -14,6 +15,11 @@ void Geodash3::init(GLFWwindow *&win, const char *title)
 		exit(-1);
 	}
 	glfwMakeContextCurrent(win);	
+
+	GLFWimage icons[1];
+	icons[0].pixels = SOIL_load_image("res/geodash3.png", &icons[0].width, &icons[0].height, 0, SOIL_LOAD_RGBA);
+	glfwSetWindowIcon(win, 1, icons);
+	SOIL_free_image_data(icons[0].pixels);
 
 	//Initialize glad
 	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
