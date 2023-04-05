47bbcacf00a596f47199534352e45799751ab6ea
jli <coderguy50@gmail.com>
2023-04-05 09:31:18 -0500

    changed rotation speed of camera in editor

--- editor/src/EngineUpdate.cpp
+++ editor/src/EngineUpdate.cpp
@@ -8,13 +8,8 @@ void Geodash3::Engine::m_Update()
 	glfwGetCursorPos(this->m_gameWindow, &tempMouseX, &tempMouseY);
 	if(glfwGetInputMode(this->m_gameWindow, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)
 	{
-#ifndef WINDOWS
-		this->m_cameraRotationSpeed = 0.1f * (float)floorf(tempMouseX - this->m_mouseX);
-		this->m_cameraPitchSpeed = -0.1f * (float)floorf(tempMouseY - this->m_mouseY);
-#else
-		this->m_cameraRotationSpeed = 1.6f * (float)(tempMouseX - this->m_mouseX);
-		this->m_cameraPitchSpeed = -1.6f * (float)(tempMouseY - this->m_mouseY);
-#endif
+		this->m_cameraRotationSpeed = 0.2f * (float)floorf(tempMouseX - this->m_mouseX);
+		this->m_cameraPitchSpeed = -0.2f * (float)floorf(tempMouseY - this->m_mouseY);
 
 		if(this->m_camera.rotation.x < -3.14159f / 2.0f)
 			this->m_camera.rotation.x = -3.14159f / 2.0f;
